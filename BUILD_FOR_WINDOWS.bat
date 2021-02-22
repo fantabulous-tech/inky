982f174990a6399a3bf1a62030baf72b1af25167
Fantabulous <fan@fantabulous.tech>
2021-02-22 17:39:49 +1300

    Simplify build

--- BUILD_FOR_WINDOWS.bat
+++ BUILD_FOR_WINDOWS.bat
@@ -18,7 +18,7 @@ REM Build
 call electron-packager app Inky --platform=win32  --arch=x64 --icon=resources/Icon1024.png.ico --prune --asar.unpackDir="main-process/ink" --ignore="inklecate_mac" --win32metadata.ProductName="Inky" --win32metadata.CompanyName="inkle Ltd" --win32metadata.FileDescription="Inky" --win32metadata.OriginalFilename="Inky" --win32metadata.InternalName="Inky"
 
 REM zip if powershell is available
-powershell /? >nul 2>&1 && (
-	md ReleaseUpload
-	powershell Compress-Archive Inky-win32-x64 ReleaseUpload\Inky-win32-x64.zip 
-)
+@REM powershell /? >nul 2>&1 && (
+@REM 	md ReleaseUpload
+@REM 	powershell Compress-Archive Inky-win32-x64 ReleaseUpload\Inky-win32-x64.zip 
+@REM )
